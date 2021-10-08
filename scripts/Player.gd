af7045ebe3fbfb2a15588334e7fff1863369b8b8
Nilton Calegari <nilton_calegari@dell.com>
2021-10-08 15:01:36 -0300

    add animation and flip

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -27,8 +27,25 @@ func _process(delta: float) -> void:
 		velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
+	update_animation()
+	
 func get_movement_vector():
 	var moveVector = Vector2.ZERO
 	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	return moveVector
+	
+func update_animation():
+	var moveVec = get_movement_vector()
+	
+	if (!is_on_floor()):
+		$AnimatedSprite.play("jump")
+	elif (moveVec.x != 0):
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+	
+	if (moveVec.x != 0):
+		$AnimatedSprite.flip_h = true if moveVec.x > 0 else false
+		
+	
