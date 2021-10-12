4667491344d468431a9a1c72d33dc328f8afd6b1
Nilton Calegari <nilton_calegari@dell.com>
2021-10-12 14:29:32 -0300

    Added 1 sample level to test the feel

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -12,9 +12,10 @@ var maxHorizontalSpeed = 140
 var maxDashSpeed = 500
 var minDashSpeed = 200
 var horizontalAcceleration = 2000
-var jumSpeed = 360
+var jumSpeed = 320
 var jumpTerminationMultiplier = 4
 var hasDoubleJump = false
+var hasDash = false
 var currentState = State.NORMAL
 var isStateNew = true
 
@@ -69,9 +70,11 @@ func process_normal(delta):
 		
 	if (is_on_floor()):
 		hasDoubleJump = true
+		hasDash = true
 		
-	if (Input.is_action_just_pressed("dash")):
+	if (hasDash && Input.is_action_just_pressed("dash")):
 		call_deferred("change_state", State.DASHING)
+		hasDash = false
 		
 	update_animation()
 	
