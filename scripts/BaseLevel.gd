679dca592e530ff282ba8dca6742db5f2396ac51
Nilton Calegari <nilton_calegari@dell.com>
2021-10-12 15:42:06 -0300

    Added logic to coin collect

--- scripts/BaseLevel.gd
+++ scripts/BaseLevel.gd
@@ -1,12 +1,25 @@
 extends Node
 
+signal coin_total_changed
+
 var playerScene = preload("res://scenes/Player.tscn")
 var spawnPosition = Vector2.ZERO
 var currentPlayerNode = null
+var totalCoins = 0
+var collectedCoins = 0
 
 func _ready():
 	spawnPosition = $Player.global_position
 	register_player($Player)
+	coin_total_change(get_tree().get_nodes_in_group("coin").size())
+
+func coin_collected():
+	collectedCoins += 1
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
+	
+func coin_total_change(newTotal):
+	totalCoins = newTotal
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
 
 func register_player(player):
 	currentPlayerNode = player
--- scripts/Coin.gd
+++ scripts/Coin.gd
@@ -6,6 +6,8 @@ func _ready():
 func on_area_entered(area2d):
 	$AnimationPlayer.play("pickup")
 	call_deferred("disable_pickup")
+	var baseLevel = get_tree().get_nodes_in_group("base_level")[0]
+	baseLevel.coin_collected()
 	
 func disable_pickup():
 	$Area2D/CollisionShape2D.disabled = true
