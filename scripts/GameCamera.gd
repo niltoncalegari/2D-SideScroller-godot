db8624484895ffb8bf1a376fe89106c22f8779fa
Nilton Calegari <nilton_calegari@dell.com>
2021-10-13 17:09:32 -0300

    added camera shake

--- scripts/GameCamera.gd
+++ scripts/GameCamera.gd
@@ -3,13 +3,38 @@ extends Camera2D
 var targetPosition = Vector2.ZERO
 
 export(Color, RGB) var  backgroundColor
+export(OpenSimplexNoise) var shakeNoise
+
+var xNoiseSampleVector = Vector2.RIGHT
+var yNoiseSampleVector = Vector2.DOWN
+var xNoiseSamplePosition = Vector2.ZERO
+var yNoiseSamplePosition = Vector2.ZERO
+var noiseSampleTravelRate = 500
+var maxShakeOffset = 6
+var currentShakePercentage = 0
+var shakeDecay = 4
 
 func _ready() -> void:
 	 VisualServer.set_default_clear_color(backgroundColor)
 
 func _process(delta: float) -> void:
 	acquire_target_position()
+	
 	global_position = lerp(targetPosition, global_position, pow(2, -15 * delta))
+		
+	if (currentShakePercentage > 0):
+		xNoiseSamplePosition += xNoiseSampleVector * noiseSampleTravelRate * delta
+		yNoiseSamplePosition += yNoiseSampleVector * noiseSampleTravelRate * delta
+		var xSample = shakeNoise.get_noise_2d(xNoiseSamplePosition.x, xNoiseSamplePosition.y)
+		var ySample = shakeNoise.get_noise_2d(yNoiseSamplePosition.x, yNoiseSamplePosition.y)
+		
+		var calculatedOffset = Vector2(xSample, ySample) * maxShakeOffset * pow(currentShakePercentage, 2)
+		offset = calculatedOffset
+		
+		currentShakePercentage = clamp(currentShakePercentage - shakeDecay * delta, 0, 1)
+
+func apply_shake(percentage):
+	currentShakePercentage = clamp(currentShakePercentage + percentage, 0, 1)
 
 func acquire_target_position():
 	var players = get_tree().get_nodes_in_group("player")
