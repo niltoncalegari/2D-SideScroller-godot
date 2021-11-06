def6d68d3d2b59e6acb31722a73bdd2b7f031d52
Nilton Calegari <calegari.lk@gmail.com>
2021-11-06 14:36:03 -0300

    Added tutorial signals

---
+++ scripts/TutorialSign.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+export(String, MULTILINE) var text
+
+func _ready():
+	$PanelContainer/MarginContainer/Label.text = text
+	$PanelContainer.visible = false
+	
+	$Area2D.connect("area_entered", self, "on_area_entered")
+	$Area2D.connect("area_exited", self, "on_area_exited")
+	
+func on_area_entered(_area2d):
+	$PanelContainer.visible = true
+	$Sprite.frame = 1
+	
+func on_area_exited(_area2d):
+	$PanelContainer.visible = false
+	$Sprite.frame = 0
